e8ce4f395c256a5d9349821e5d8d5cea2943bee6
ivicaca <ivicaca@hotmail.com>
2016-08-18 18:20:26 +0300

    Primer koda, treba dati prave parametre u fajl.

---
+++ MySQLtoJSON.php
@@ -0,0 +1,34 @@
+<?php
+
+// Ovde postavi prave parametre za logovanje na bazu
+$dbhost = 'ime hosta';
+$dbuser = 'username';
+$dbpass = 'password';
+$dbname = 'ime baze';
+
+//kreiranje linka na bazu
+  $dblink = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+//Provera konekcije
+  if ($dblink->connect_errno) {
+     printf("Nema konekcije na bazu");
+     exit();
+  }
+
+//deset redova
+  $result = $dblink->query("SELECT * FROM imeTabele LIMIT 10");
+
+//Matrica za rezultate
+  $dbdata = array();
+
+//Rezultati u matricu
+  while ( $row = $result->fetch_assoc())  {
+	$dbdata[]=$row;
+  }
+
+//Izlaz na ekran ili u fajl
+//echo json_encode($dbdata);
+    $fp = fopen('PolenData.json', 'w');
+    fwrite($fp, json_encode($dbdata));
+    fclose($fp);
+?>
